6cfb1cb34f5e6167dc58ab116530fca99175e456
ugsfume <86786290+ugsfume@users.noreply.github.com>
2023-11-14 13:09:08 +0000

    updated robotHW for ackermann
    not working

--- ros/gra_base/src/control/hardware/robot_hw.h
+++ ros/gra_base/src/control/hardware/robot_hw.h
@@ -7,78 +7,101 @@
 #include <controller_manager/controller_manager.h>
 #include <sensor_msgs/JointState.h>
 
-#define NUM_MOTORS 3
+#define NUM_DRIVE_JOINTS 1
+#define NUM_STEER_JOINTS 1
 
 class RobotHW : public hardware_interface::RobotHW {
-    private:
-        hardware_interface::JointStateInterface jnt_state_interface; // This is the interface for reading the state of the joints
-        hardware_interface::VelocityJointInterface jnt_vel_interface; // This is the interface for commanding the joints
+private:
+    hardware_interface::JointStateInterface jnt_state_interface;
+    hardware_interface::VelocityJointInterface jnt_vel_interface;
+    hardware_interface::PositionJointInterface jnt_pos_interface;
 
-        // We then need to store the state and command of the joints
-        double cmd[3] = {0.0, 0.0, 0.0};
-        double pos[3] = {0.0, 0.0, 0.0};
-        double vel[3] = {0.0, 0.0, 0.0};
-        double eff[3] = {0.0, 0.0, 0.0};
+    double cmd_drive[NUM_DRIVE_JOINTS] = {0.0};
+    double pos_drive[NUM_DRIVE_JOINTS] = {0.0};
+    double vel_drive[NUM_DRIVE_JOINTS] = {0.0};
+    double eff_drive[NUM_DRIVE_JOINTS] = {0.0};
 
-        // ROS node
-        ros::Publisher wheel_vel_pub;
-        ros::Subscriber wheel_state_sub;
+    double cmd_steer[NUM_STEER_JOINTS] = {0.0};
+    double pos_steer[NUM_STEER_JOINTS] = {0.0};
+    double vel_steer[NUM_STEER_JOINTS] = {0.0};
+    double eff_steer[NUM_STEER_JOINTS] = {0.0};
 
-        void baseStateCallback(const gra_base::BaseState& msg) {
-            for (int i = 0; i < NUM_MOTORS; i++) {
-                pos[i] = msg.states[i].position;
-                vel[i] = msg.states[i].velocity;
-                eff[i] = msg.states[i].output;
-            }
+    // ROS node
+    ros::Publisher wheel_vel_pub;
+    ros::Subscriber wheel_state_sub;
+
+    void baseStateCallback(const gra_base::BaseState& msg) {
+        // Update joint states from robot feedback
+        // This should be adjusted according to how your robot feedback is structured
+        for (int i = 0; i < NUM_DRIVE_JOINTS; i++) {
+            pos_drive[i] = msg.states[i].position;
+            vel_drive[i] = msg.states[i].velocity;
+            eff_drive[i] = msg.states[i].output;
+        }
+        for (int i = 0; i < NUM_STEER_JOINTS; i++) {
+            pos_steer[i] = msg.states[NUM_DRIVE_JOINTS + i].position;
+            vel_steer[i] = msg.states[NUM_DRIVE_JOINTS + i].velocity;
+            eff_steer[i] = msg.states[NUM_DRIVE_JOINTS + i].output;
+        }
+    }
+
+    float control_frequency;
+
+public:
+    ros::NodeHandle nh;
+    ros::Rate* control_rate;
+    RobotHW() {
+        ROS_INFO("Initializing hardware interface");
+
+        // Register drive joints
+        for (int i = 0; i < NUM_DRIVE_JOINTS; i++) {
+            std::string joint_name = "rear_wheel_joint";
+            hardware_interface::JointStateHandle state_handle_drive(joint_name, &pos_drive[i], &vel_drive[i], &eff_drive[i]);
+            jnt_state_interface.registerHandle(state_handle_drive);
+
+            hardware_interface::JointHandle vel_handle(jnt_state_interface.getHandle(joint_name), &cmd_drive[i]);
+            jnt_vel_interface.registerHandle(vel_handle);
         }
 
-        float control_frequency;
-    
-    public:
-        ros::NodeHandle nh;
-        ros::Rate* control_rate;
-        RobotHW() {
-            ROS_INFO("Initializing hardware interface");
+        // Register steering joints
+        for (int i = 0; i < NUM_STEER_JOINTS; i++) {
+            std::string joint_name = "front_steer_joint";
+            hardware_interface::JointStateHandle state_handle_steer(joint_name, &pos_steer[i], &vel_steer[i], &eff_steer[i]);
+            jnt_state_interface.registerHandle(state_handle_steer);
 
-            // Register the joints with the state and velocity interfaces
-            for (int i = 0; i < NUM_MOTORS; i++) {
-                std::string joint_name = "wheel_" + std::to_string(i + 1) + "_joint";
-                hardware_interface::JointStateHandle state_handle(joint_name, &pos[i], &vel[i], &eff[i]);
-                jnt_state_interface.registerHandle(state_handle);
-            }
-
-            registerInterface(&jnt_state_interface);
-
-            for (int i = 0; i < NUM_MOTORS; i++) {
-                std::string joint_name = "wheel_" + std::to_string(i + 1) + "_joint";
-                hardware_interface::JointHandle vel_handle(jnt_state_interface.getHandle(joint_name), &cmd[i]);
-                jnt_vel_interface.registerHandle(vel_handle);
-            }
-            registerInterface(&jnt_vel_interface);
-
-            // Initialize the publisher and subscriber
-            wheel_vel_pub = nh.advertise<gra_base::BaseSetpoint>("base_setpoint", 1000);
-            wheel_state_sub = nh.subscribe("base_state", 1000, &RobotHW::baseStateCallback, this);
-
-            // Get the controller frequency
-            nh.param<float>("control_frequency", control_frequency, 100.0);
-
-            // Initialize the control rate
-            control_rate = new ros::Rate(control_frequency);
-
-            ROS_INFO("Hardware interface initialized");
+            hardware_interface::JointHandle pos_handle(jnt_state_interface.getHandle(joint_name), &cmd_steer[i]);
+            jnt_pos_interface.registerHandle(pos_handle);
         }
-        void write() {
-            // Publish the target wheel velocities
-            gra_base::BaseSetpoint msg;
-            for (int i = 0; i < NUM_MOTORS; i++) {
-                msg.setpoints[i].velocity = cmd[i];
-            }
-            wheel_vel_pub.publish(msg);
+
+        registerInterface(&jnt_state_interface);
+        registerInterface(&jnt_vel_interface);
+        registerInterface(&jnt_pos_interface);
+
+        // Initialize the publisher and subscriber
+        wheel_vel_pub = nh.advertise<gra_base::BaseSetpoint>("base_setpoint", 1000);
+        wheel_state_sub = nh.subscribe("base_state", 1000, &RobotHW::baseStateCallback, this);
+
+        // Get the controller frequency
+        nh.param<float>("control_frequency", control_frequency, 100.0);
+
+        // Initialize the control rate
+        control_rate = new ros::Rate(control_frequency);
+
+        ROS_INFO("Hardware interface initialized");
+    }
+    void write() {
+        // Publish the target wheel velocities and steering commands
+        gra_base::BaseSetpoint msg;
+        for (int i = 0; i < NUM_DRIVE_JOINTS; i++) {
+            msg.setpoints[i].velocity = cmd_drive[i];
+            // Add steering commands if needed
         }
-        void read() {
-        }
-        ~RobotHW() {
-            delete control_rate;
-        }
+        wheel_vel_pub.publish(msg);
+    }
+    void read() {
+        // Read feedback from the robot and update joint states
+    }
+    ~RobotHW() {
+        delete control_rate;
+    }
 };
--- ros/gra_base/src/control/hardware_parameter_server/hardware_parameter_server.h
+++ ros/gra_base/src/control/hardware_parameter_server/hardware_parameter_server.h
@@ -3,7 +3,8 @@
 #include "gra_base/BaseAdaptiveState.h"
 #include "gra_base/BaseState.h"
 
-#define NUM_MOTORS 4
+#define NUM_DRIVE_JOINTS 2
+#define NUM_STEER_JOINTS 1
 
 class HardwareParameterServer {
     private:
@@ -12,36 +13,47 @@ class HardwareParameterServer {
         ros::Publisher base_parameters_pub;
         ros::Publisher base_adaptive_state_pub;
         ros::NodeHandle _nh;
-        // Generic type method for initializing a parameter
+        
+        // Generic method for initializing a parameter
         template <typename T>
-        void initParam(T& param, int motor_id, std::string name, T default_value) {
-            _nh.param<T>("motor_" + std::to_string(motor_id + 1) + "/" + name, param, default_value);
+        void initParam(T& param, std::string name, T default_value) {
+            _nh.param<T>(name, param, default_value);
         }
-        // Create integer array of size NUM_MOTORS to store motor mode
-        int motor_mode[NUM_MOTORS] = {0};
+        
+        // Parameters for Ackermann steering
+        float wheel_radius;
+        float wheel_base;
+        float steering_limit;
+
     public:
         HardwareParameterServer() : _nh("hardware_parameter_server") {
-            
-            // Initialize the publisher and subscriber
+            // Initialize the publisher
             base_parameters_pub = _nh.advertise<gra_base::BaseParameters>("/base_parameters", 1000);
             base_adaptive_state_pub = _nh.advertise<gra_base::BaseAdaptiveState>("/base_adaptive_state", 1000);
 
-            for (int i = 0; i < NUM_MOTORS; i++) {
-                initParam<float>(base_parameters_msg.parameters[i].p_in, i, "p_in", 35.0);
-                initParam<float>(base_parameters_msg.parameters[i].i_in, i, "i_in", 0.0015);
-                initParam<float>(base_parameters_msg.parameters[i].d_in, i, "d_in", 0.0);
-                initParam<float>(base_parameters_msg.parameters[i].bias, i, "bias", 50.0);
-                initParam<int>(motor_mode[i], i, "control_mode", 3);
-                initParam<float>(base_adaptive_state_msg.adaptive_states[i].i_accumulator, i, "i_accumulator_initial", 20.0);
+            // Initialize parameters
+            initParam<float>(wheel_radius, "wheel_radius", 0.25); 
+            initParam<float>(wheel_base, "wheel_base", 1.54);     // distance between front wheels and rear wheels
+            initParam<float>(steering_limit, "steering_limit", 0.785398); // 45 degrees in radians
+
+            // Initialize drive joint PID gains and other parameters
+            for (int i = 0; i < NUM_DRIVE_JOINTS; i++) {
+                initParam<float>(base_parameters_msg.parameters[i].p_in, "drive_joint_" + std::to_string(i + 1) + "/p_in", 35.0);
+                initParam<float>(base_parameters_msg.parameters[i].i_in, "drive_joint_" + std::to_string(i + 1) + "/i_in", 0.0015);
+                initParam<float>(base_parameters_msg.parameters[i].d_in, "drive_joint_" + std::to_string(i + 1) + "/d_in", 0.0);
+                // Additional parameters as needed
             }
-            
+
+            // Initialize steering joint PID gains and other parameters
+            for (int i = 0; i < NUM_STEER_JOINTS; i++) {
+                initParam<float>(base_adaptive_state_msg.adaptive_states[i].i_accumulator, "steer_joint_" + std::to_string(i + 1) + "/i_accumulator_initial", 20.0);
+                // Additional parameters as needed
+            }
+
             ROS_INFO("Waiting for /base_state to be published...");
             ros::topic::waitForMessage<gra_base::BaseState>("/base_state");
             ROS_INFO("Received /base_state message, publishing parameters /base_parameters and /adaptive_state");
 
-            // Wait 1 second for the subscriber to connect
-
-            // syncBoolToUint();
             base_parameters_pub.publish(base_parameters_msg);
             base_adaptive_state_pub.publish(base_adaptive_state_msg);
         }
